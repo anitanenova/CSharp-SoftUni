222eff5418e4769e098221c1804cb4127dab5f92
anitanenova <67379136+anitanenova@users.noreply.github.com>
2021-01-03 22:14:25 +0200

    01

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01. Honeycombs
+{
+    class Program
+{
+        static void Main(string[] args)
+        {
+
+            int coutBeeps = int.Parse(Console.ReadLine());
+            int countFlowers = int.Parse(Console.ReadLine());
+
+            double sumGramaMed = coutBeeps * 0.21 * countFlowers;
+            double restHoney = sumGramaMed % 100;
+
+            double countPiti  = sumGramaMed - restHoney;
+
+            Console.WriteLine($"{countPiti/100} honeycombs filled.");
+            Console.WriteLine($"{restHoney:F2} grams of honey left.");
+        
+        }
+    }
+}
